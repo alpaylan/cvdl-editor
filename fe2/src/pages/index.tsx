3562a89db75ff7530d2c9698f10e86498090b771
alpaylan <alpkeles99@gmail.com>
2023-11-03 00:09:42 -0400

    make deployable

--- fe2/src/pages/index.tsx
+++ fe2/src/pages/index.tsx
@@ -8,21 +8,14 @@ import 'react-pdf/dist/Page/TextLayer.css';
 import { FontDict } from 'cvdl-ts/src/AnyLayout';
 import { render as pdfRender } from 'cvdl-ts/src/PdfLayout';
 import { RemoteStorage } from 'cvdl-ts/src/RemoteStorage';
+import { LocalStorage } from 'cvdl-ts/src/LocalStorage';
 import { Storage } from 'cvdl-ts/src/Storage';
 import { ItemContent, Resume } from 'cvdl-ts/src/Resume';
 import { LayoutSchema } from 'cvdl-ts/src/LayoutSchema';
 import { ResumeLayout } from 'cvdl-ts/src/ResumeLayout';
 import { DataSchema } from 'cvdl-ts/src/DataSchema';
+// @ts-ignore
 import * as pdfjsLib from 'pdfjs-dist/webpack';
-
-
-const MapItems = (map: Map<K, V>) => {
-  const items: [K, V][] = []
-  for (const [key, value] of map) {
-    items.push([key, value])
-  }
-  return items;
-}
 
 function App() {
   console.log = function () { }
@@ -30,8 +23,7 @@ function App() {
   console.error = function () { }
   console.debug = function () { }
 
-  const [storage, setStorage] = useState<Storage>(new RemoteStorage("http://127.0.0.1:8000"));
-
+  const [storage, setStorage] = useState<Storage>(new LocalStorage());
   const [numPages, setNumPages] = useState<number>();
   const [pdf, setPdf] = useState<string | null>(null);
   const [resume, setResume] = useState<string>("resume2");
@@ -41,12 +33,11 @@ function App() {
   const [dataSchemas, setDataSchemas] = useState<DataSchema[] | null>(null)
   const [fontDict, setFontDict] = useState<FontDict>(new FontDict());
   const [debug, setDebug] = useState<boolean>(false);
-  const [renderTarget, setRenderTarget] = useState<"pdf" | "img">("pdf");
-  const [img, setImg] = useState<string | null>(null);
 
   useEffect(() => {
     require('../registerStaticFiles.js');
-  }, []);
+    storage.initiate_storage();
+  });
 
   useEffect(() => {
     storage.load_resume(resume).then((data) => {
@@ -76,38 +67,45 @@ function App() {
     data_schema_loader().then((schemas) => setDataSchemas(schemas))
     layout_schema_loader().then((schemas) => setLayoutSchemas(schemas))
     resume_layout_loader().then((layout) => setResumeLayout(layout))
+
   }, [resumeData, storage]);
 
 
 
   useEffect(() => {
-    if (renderTarget === "pdf") {
-      const start_time = performance.now();
-      pdfRender({
-        resume_name: resume,
-        resume: resumeData!,
-        storage,
-        fontDict,
-        debug
-      }).then(({ blob, fontDict, pages }) => {
-        setPdf(window.URL.createObjectURL(blob));
-        setFontDict(fontDict);
+    const start_time = performance.now();
+    pdfRender({
+      resume_name: resume,
+      resume: resumeData!,
+      storage,
+      fontDict,
+      debug
+    }).then(({ blob, fontDict, pages }) => {
+      setPdf(window.URL.createObjectURL(blob));
+      setFontDict(fontDict);
 
-        const end_time = performance.now();
-        console.info("Rendering pdf took " + (end_time - start_time) + "ms");
-      });
-    }
-  }, [resume, fontDict, debug, storage, renderTarget, resumeData]);
+      const end_time = performance.now();
+      console.info("Rendering pdf took " + (end_time - start_time) + "ms");
+    });
+
+  }, [resume, fontDict, debug, storage, resumeData]);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setNumPages(numPages);
   }
+
+  const saveResume = () => {
+    if (!resumeData) {
+      return;
+    }
+    storage.save_resume("resume2", resumeData);
+  }
+
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
       <div style={{ display: "flex", width: "700px" }}>
         <div style={{ display: "flex", flexDirection: "column" }}>
-          <button onClick={() => setResume("resume2")} >Set resume 2</button>
-          <button onClick={() => setResume("resume4")} >Set resume 4</button>
+          <button onClick={saveResume} >Save</button>
           <button onClick={() => setDebug(!debug)} >Invert Debug</button>
           <b >Sections</b>
           {(resumeData && layoutSchemas) &&
@@ -146,6 +144,7 @@ function App() {
                                   <input type="text" onChange={(e) => {
                                     item.set(field.name, ItemContent.fromJson(e.target.value));
                                     setResumeData(new Resume(resumeData.layout, resumeData.sections));
+                                    // @ts-ignore
                                   }} defaultValue={ItemContent.toString(item.get(field.name) ?? ItemContent.None())} />
                                 </div>
                               )
