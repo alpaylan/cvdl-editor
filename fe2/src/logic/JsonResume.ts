f7c3b539e96f3d97d1207e4b322fade09c3d6775
alpaylan <alpkeles99@gmail.com>
2024-05-12 18:57:02 +0300

    add work wip

--- fe2/src/logic/JsonResume.ts
+++ fe2/src/logic/JsonResume.ts
@@ -136,16 +136,57 @@ const jsonResumeBasicsLayout: LayoutSchema = new LayoutSchema(
     )
 );
 
+const jsonResumeWork: DataSchema = new DataSchema("json-resume-work",
+    [],
+    [
+        // name?: string;
+        { name: 'name', data_type: { tag: 'String' } },
+        // position?: string;
+        { name: 'position', data_type: { tag: 'String' } },
+        // url?: string;
+        { name: 'url', data_type: { tag: 'String' } },
+        // startDate?: string;
+        { name: 'startDate', data_type: { tag: 'Date' } },
+        // endDate?: string;
+        { name: 'endDate', data_type: { tag: 'Date' } },
+        // summary?: string;
+        { name: 'summary', data_type: { tag: 'String' } },
+        // highlights?: string[];
+        { name: 'highlights', data_type: { tag: 'List', value: { tag: 'String' } } }
+    ]
+)
+
+const jsonResumeWorkLayout: LayoutSchema = new LayoutSchema("json-resume-work", "json-resume-work",
+    new SectionLayout(Elem.default_().with_item("Work")),
+    new SectionLayout(
+        new Stack([
+            new SectionLayout(Elem.default_().as_ref().with_item("name").with_width(Width.percent(100)).with_alignment("Center").with_font(new Font("Exo", 16, "Bold", "Normal", "Local"))),
+            new SectionLayout(Row.default_().with_elements([
+                new SectionLayout(Elem.default_().as_ref().with_item("position").with_font(new Font("Exo", 12, "Medium", "Normal", "Local")).with_width(Width.percent(50))),
+                new SectionLayout(Elem.default_().as_ref().with_item("startDate").with_font(new Font("Exo", 12, "Medium", "Normal", "Local")).with_width(Width.percent(50)).with_alignment("Right"))
+            ]))
+        ])
+    )
+)
+
+
+
+
 const get = (obj: any, key: string): ItemContent => {
     return obj[key] ? { tag: "String", value: obj[key] } : { tag: "String", value: "" };
 }
 
 
+const randomId = () => {
+    return Math.random().toString(36).substring(7);
+}
 
 export const convert = (json: JsonResume): Resume => {
     const storage = new LocalStorage();
     storage.save_data_schema(jsonResumeBasics);
     storage.save_layout_schema(jsonResumeBasicsLayout);
+    storage.save_data_schema(jsonResumeWork);
+    storage.save_layout_schema(jsonResumeWorkLayout);
 
     const basics: ResumeSection = new ResumeSection();
     basics.section_name = "Basics";
@@ -157,5 +198,25 @@ export const convert = (json: JsonResume): Resume => {
         ["phone", get(json.basics, "phone")]
     ]);
 
-    return new Resume("SingleColumnSchema", [basics]);
+    const work: ResumeSection = new ResumeSection();
+    work.section_name = "Work Experience";
+    work.data_schema = "json-resume-work";
+    work.layout_schema = "json-resume-work";
+    json.work?.map((w) => new Map([
+        ["name", get(w, "name")],
+        ["position", get(w, "position")],
+        ["url", get(w, "url")],
+        ["startDate", get(w, "startDate")],
+        ["endDate", get(w, "endDate")],
+        ["summary", get(w, "summary")],
+        ["highlights", { tag: "List", value: (w.highlights?.map((h: string) => ({ tag: "String", value: h })) || []) } as ItemContent]
+    ])
+    ).forEach((w) => {
+        work.items.push({
+            id: randomId(),
+            fields: w
+        });
+    });
+
+    return new Resume("SingleColumnSchema", [basics, work]);
 }
