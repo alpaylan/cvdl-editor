af2199ecb372de9c4aa3000ef3ebc96bfe7313db
alpaylan <alpkeles99@gmail.com>
2024-05-16 01:24:08 +0300

    page title

--- fe2/src/app/page.tsx
+++ fe2/src/app/page.tsx
@@ -1,4 +1,12 @@
 import HomePage from '@/components/HomePage';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: "Tail",
+    description: "Next Generation Document Builder",
+    keywords: ["document", "resume", "builder", "app"],
+};
+
 
 const App = () => {
     return <HomePage />;
